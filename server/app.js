0da04b2e0c88e4a24e28bb680f20ccfbef3ef57d
Sean Yeh <sean.yeh@gmx.com>
2014-04-05 18:59:09 -0400

    Add server

---
+++ server/app.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var https = require('https');
+
+function Server(port){
+    var fs = require('fs');
+
+    var express = require("express");
+    var expressApp = express();
+
+    expressApp.use(express.static('public'));
+    this.io = require('socket.io').listen(expressApp.listen(port));
+    this.io.set('log level', 1); // reduce logging
+
+    this.users = {};
+
+
+    /**
+     * Socket events
+     */
+    var self = this;
+    self.io.sockets.on('connect', function(socket){
+        console.log("Received connection: " + socket);
+        socket.emit('auth',{});
+
+        socket.on('connect', function(data){
+            self.users[socket.id] = {id:socket.id, name:data.name};
+        });
+
+        socket.on('move', function(data){
+            socket.SENDALL('move',data);
+        });
+
+
+        socket.on('disconnect', function() {
+            console.log("Disconnect!");
+            socket.SENDALL('disconnect', {id:socket.id});
+            delete self.users[socket.id];
+        });
+    });
+
+}
+
+
+
+// Main
+
+var port = 9501;
+if (process.argv[2]){
+    port = process.argv[2];
+}
+console.log("Running on port: " + port);
+
+var s = new Server(port);
+
+
